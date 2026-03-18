51ea7b2264c0753e01dbd727e0ca49374102b6b6
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-03-18 19:17:26 -0400

    Create an issuer-lambda package structure

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ZeroVerify/issuer-lambda
+
+go 1.25.5
+
+require github.com/aws/aws-lambda-go v1.53.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/ZeroVerify/issuer-lambda/internal/handler"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	h := handler.NewHandler()
+	lambda.Start(h.Handle)
+}
